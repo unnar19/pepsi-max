4475b52eb1fb38d3fb60c734f1e8d4d13c204dac
Eythor Mikael <eythor108@gmail.com>
2021-11-30 14:44:47 +0000

    Login feature implemented in Logic_wrapper, Employee, Data_wrapper
    Get_employee_list implemented in Logic_wrapper, Employee, Data_wrapper

--- app/Data/Data_wrapper.py
+++ app/Data/Data_wrapper.py
@@ -1,3 +1,11 @@
+import Log_employee
+
 class Data_wrapper:
     def __init__(self) -> None:
-        pass
+        self.log_employee = Log_employee()
+
+    def authenticate_employee_username(self, username):
+        return self.log_employee.authenticate_username(username)
+
+    def get_all_employee_fields(self, fields):
+        return self.log_employee.get_all_from_fields(fields)
