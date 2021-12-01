92e8d26e1d9b13f5b8679bb65425c01ca984b5b2
unnar19 <unnar19@ru.is>
2021-12-01 15:07:02 +0000

    formatting class usage

--- app/UI/MainUI.py
+++ app/UI/MainUI.py
@@ -1,73 +1,30 @@
-import os
-
+from FormatUI import FormatUI
 class MainUI:
     def __init__(self):
         self.isloggedin = False
-        self.command_list = ['Email','Password','Log-in']
-        self.view = ['[empty]','[empty]', '']
-        self.hiddenformat = [1]
-        self.length = len(self.command_list)
-        self.location = 'Log-in'
-        self.line = "------------------------------------------------------------------------------------------------------------------------"
+        self.format = FormatUI()
 
-    def header(self):
-        return '{:<40}NaN Air | Dividing by zero every day!\n'.format('') + self.line
-
-    def footer(self, comment_str):
-        return self.line + "\n {}\n".format(comment_str) + self.line
-
-    def getinput(self):
-        return input(" Input: ")
-
-    def changeView(self, index, some_str):
-        if self.view[index] != '':
-            if len(some_str) == 0:
-                self.view[index] = '[empty]'
-            else:
-                if len(some_str) <= 25:
-                    self.view[index] = '[{}]'.format(some_str)
-                else:
-                    self.view[index] = '[{}...]'.format(some_str[:25])
-
-    def checkInput(self, input_str):
-        if input_str == '1':
-            return self.printScreen('Enter email address',editable=False, editing=1)
-        elif input_str == '2':
-            return self.printScreen('Enter password',editable=False, editing=2)
-        elif input_str == '3':
-            os.system('cls')
-            print('Authenticate user, if exists log-in, else repeat')
-        else:
-            return self.printScreen()
-
-    def hideString(self, input_str):
-        hidden_str = ''
-        for _ in range(len(input_str)):
-            hidden_str += '*'
-        return hidden_str
-
-    def printScreen(self, comment_str='Please select an option',editable=True, editing=0):
-        os.system('cls')
-        print(self.header())
-        print(" {}\n".format(self.location))
-        for i in range(self.length):
-            print("\t{}. {:<20} {}".format(str(i+1),self.command_list[i],self.view[i]))
+    def make_login_screen(self):
+        self.format.subtitle = 'Log-in screen'
+        self.format.edit_commands(['Email','Password','Log-in'])
+        self.format.apply_styles([0,0,1])
+        self.footers = []
         
-        for i in range(self.length,21):
-            print('')
-        
-        print(self.footer(comment_str))
-        input_str = self.getinput()
-        if editable:
-            self.checkInput(input_str)
-        else:
-            if editing-1 in self.hiddenformat:
-                input_str = self.hideString(input_str)
-                self.changeView(editing-1, input_str)
-            else:
-                self.changeView(editing-1, input_str)
-            return self.printScreen()
-
 if __name__ == "__main__":
-    some = MainUI()
-    some.printScreen()
+    list_of_comments = ['Enter email address', 'Enter password']
+    main = MainUI()
+    main.make_login_screen()
+    while not main.isloggedin:
+        main.format.edit_comment('Select an option')
+        main.format.print_screen()
+        selection = main.format.get_input('Input')
+        if selection == '1' or selection == '2': # Text box
+            main.format.edit_comment(list_of_comments[int(selection)-1])
+            main.format.print_screen()
+            text = main.format.get_input('Text input')
+            main.format.change_text_box(int(selection),text)
+            main.format.print_screen()
+        if selection == '3':
+            main.isloggedin = True
+    main.format.clear_screen()
+    print('authenticate the user')
