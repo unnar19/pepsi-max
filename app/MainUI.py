c2fffa8620c855a7d19867d1329449c49c800f93
unnar19 <unnar19@ru.is>
2021-12-02 19:43:25 +0000

    add listUI menu for employees

--- app/MainUI.py
+++ app/MainUI.py
@@ -1,14 +1,15 @@
 from UI.EmployeeUI import EmployeeUI
 from UI.InteractionsUI import InteractionsUI
+from UI.ListUI import ListUI
 
 class MainUI:
     def __init__(self):
         self.isloggedin = False
         self.emp = EmployeeUI()
+        self.listing = ListUI()
         first_login = True
         
         while True:
-            
             self.isloggedin, self.id, self.role, self.name = self.emp.login_screen(first_login)
             if not self.isloggedin:
                 first_login = False
@@ -16,11 +17,11 @@ class MainUI:
                 # Make menu screen
                 first_login = True
                 self.isloggedin = self.menu_screen()
-                
     
     def menu_screen(self):
         screen = InteractionsUI()
         logged_str = f'Logged in as: {self.name}'
+        screen.format.subtitle = 'Menu'
         screen.format.title += f'{logged_str:>42}'
         screen.format.edit_commands(['Employees','Real Estate','Maintenance','Contractors','Log-out'])
         screen.format.apply_styles([1,1,1,1,1])
@@ -28,8 +29,7 @@ class MainUI:
         input_str = screen.get_input('Input')
         input_int, type_of_input = screen.check_type_of_input(input_str)
         if input_int == 0: #employees
-            print('Employee screen')
-            input('continue?')
+            self.listing.employees_screen(self.id, self.role, self.name)
         elif input_int == 1: #realestate
             print('Realestate screen')
             input('continue?')
