02458034c661041994f93cd6a995387e8cda0519
Eythor Mikael <eythor108@gmail.com>
2021-12-02 13:46:26 +0000

    Create LogBase which all log classes inherit

--- app/Data/LogEmployee.py
+++ app/Data/LogEmployee.py
@@ -1,128 +1,32 @@
 import json
-import jsonschema
-from Models.Schemas import employee_schema
+from Data.LogBase import LogBase
 from Exceptions import *
-import csv
-import os
 
-class LogEmployee:
+class LogEmployee(LogBase):
 
     def __init__(self) -> None:
-        self.path = "csv-files/Employee.csv" #spurning ad hafa thetta ekki hardcoded herna?
-        self.fields = list(employee_schema["data"].keys())
-
-    def post(self, data : json):
-        """
-            Data: employee schema
-            :return bool
-        """
-
-        # Parse load from LL
-        jsondata = json.loads(data)
-
-        # If file exists
-        if os.path.exists(self.path):
-
-            # Assign id's manually s.a.t. not break unique constraint
-            nextid = self.get_next_id()
-            jsondata["data"]["id"] = nextid
-
-        # Validate data from LL
-        if self.__validate_json(jsondata):
-
-            with open(self.path, 'a', newline='', encoding='utf-8') as csvfile:
-                writer = csv.DictWriter(csvfile, fieldnames=self.fields)
-
-                # Write header if DB is empty
-                if self.__is_empty():
-                    writer.writeheader()
-
-                writer.writerow(dict(jsondata['data']))
-
-            return(json.dumps({"type":True, "data": jsondata["data"]}))
-
-
-    def __is_empty(self):
-        """Returns true if csv-file is empty"""
-        return os.stat(self.path).st_size == 0
-
-    def __validate_json(self, jsonData):
-        """
-            jsonData: Json object to validate
-            :return bool 
-        """
-
-        # Use imported validation feature
-        try:
-            jsonschema.validate(instance=jsonData, schema=employee_schema)
-            return True
-
-        # Raise custom Exception for proper error handling
-        except jsonschema.exceptions.ValidationError:
-            raise IncorrectDataException
-
-    def get_all_employees_dict(self) -> dict:
-        """
-        Used by other functions in this class
-        in other languages would be "private"
-        :returns Dict of all employees
-        """
-        ret = {"type": "dict", "data": {}}
-        with open(self.path, newline='', encoding='utf-8') as empfile:
-            reader = csv.DictReader(empfile)
-            for row in reader:
-                id = row["id"]
-                emp_dict = {}
-                for key in row:
-                    emp_dict[key]=row[key]
-                ret["data"][id] = emp_dict
-        return ret
+        super().__init__("employee")
 
     def get_all(self):
-        """
-        In order not to return plain dict of data
-        returns: json dump of all employees
-        """
-        employees = self.get_all_employees_dict()
-        return json.dumps(employees)
+        return super().get_all()
 
-
-    def get_employee_by_id(self, id : int) -> json:
-        """
-        parameters: json {"id":id}
-        returns: json {"id":id, "data":{(all employee data with id)}
-        """
-        employees = self.get_all_employees_dict()
-        return json.dumps(employees["data"][id])
-
-
-    def get_next_id(self):
-        """
-        returns next ID for employees
-        """
-        max_id = 0
-        employees = self.get_all_employees_dict()
-        for employee in employees["data"]:
-            max_id = int(employee,10)
-        return int(max_id) +1
+    def post(self, data: json):
+        return super().post(data)
 
     def put(self, data: str):
-        """Deletes csv-file and rewrites it with provided data"""
+        return super().put(data)
 
-        # Parse data from LL
-        load = json.loads(data)
+    def __is_empty(self):
+        return super().__is_empty()
 
-        # Validate data from LL
-        if self.validate_json(load):
+    def __validate_json(self, jsonData):
+        return super().__validate_json(jsonData)
 
-            # Delete csv-file
-            os.remove(self.path)
+    def __get_all_dict(self) -> dict:
+        return super().__get_all_dict()
 
-            for key in load['data'].keys():
-                self.post(json.dumps({"data":load['data'][key]}))
-
-            # Return status code True, but response data is stored in LL
-            return(json.dumps({"type":True, "data": None}))
+    def __get_next_id(self):
+        return super().__get_next_id()
 
 if __name__ == "__main__":
     pass
--- app/Data/LogReport.py
+++ app/Data/LogReport.py
@@ -1,73 +1,32 @@
 import json
-import jsonschema
-from Models.Schemas import report_schema
-import csv
+from Data.LogBase import LogBase
+from Exceptions import *
 
-class LogReport:
+class LogReport(LogBase):
+
     def __init__(self) -> None:
-        self.path = "csv-files/Report.csv"
-        self.fields = list(report_schema["data"].keys())
+        super().__init__("report")
 
-    def file_report(self, data : json):
-        """
-            Data: report schema
-            :return bool
-        """
-        jsondata = json.loads(data)
-        isvalid = self.validate_json(jsondata)
-        if isvalid:
-            with open(self.path, 'a', newline='', encoding='utf-8') as csvfile:
-                writer = csv.DictWriter(csvfile, fieldnames=self.fields)
-                writer.writerow(dict(jsondata['data']))
-        else:
-            # TODO: setja propper error
-            raise ValueError()
-        
-    def validate_json(self, jsonData):
-        """
-            jsonData: Json object to validate
-            :return bool 
-        """
-        try:
-            jsonschema.validate(instance=jsonData, schema=report_schema)
-        except jsonschema.exceptions.ValidationError as err:
-            return False
-        return True
+    def get_all(self):
+        return super().get_all()
 
-    def get_all_reports_dict(self) -> dict:
-        """
-        Used by other functions in this class
-        in other languages would be "private"
-        :returns Dict of all reports
-        """
-        ret = {"type": "dict", "data": {}}
-        with open(self.path, newline='', encoding='utf-8') as report_file:
-            reader = csv.DictReader(report_file)
-            for row in reader:
-                id = row["id"]
-                report_dict = {}
-                for key in row:
-                    report_dict[key]=row[key]
-                ret["data"][id] = report_dict
-        return ret
+    def post(self, data: json):
+        return super().post(data)
 
-    def get_all_reports(self):
-        """
-        In order not to return plain dict of data
-        returns: json dump of all reports
-        """
-        report = self.get_all_reports_dict()
-        return json.dumps(report)
-    
-    def get_max_id(self):
-        """
-        returns maximum ID of reports
-        """
-        max_id = 0
-        report = self.get_all_reports_dict()
-        for report in report["data"]:
-            max_id = report
-        return max_id
+    def put(self, data: str):
+        return super().put(data)
+
+    def __is_empty(self):
+        return super().__is_empty()
+
+    def __validate_json(self, jsonData):
+        return super().__validate_json(jsonData)
+
+    def __get_all_dict(self) -> dict:
+        return super().__get_all_dict()
+
+    def __get_next_id(self):
+        return super().__get_next_id()
 
 if __name__ == "__main__":
     pass
