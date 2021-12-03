c58a395a1b20455530d7cde74078d64d56fdb232
Ísak Maron <isakh21@ru.is>
2021-12-03 14:42:38 +0000

    edit TestRealEstate.py

--- app/Tests/TestRealEstate.py
+++ app/Tests/TestRealEstate.py
@@ -3,10 +3,11 @@ from Logic.LogicAPI import LogicAPI
 import json
 
 
-new_real_estate = json.dumps({ "role": "boss",
+#this will pass in setUp
+new_real_estate1 = json.dumps({ "role": "boss",
                         "key": "real_estate",
                         "data": {
-                            "real_estate_id": "blalba",
+                            "real_estate_id": "huhuhu",
                             "address": "pound town",
                             "destination": "Nuuk",
                             "maintenance_info": "blibli",
@@ -15,27 +16,62 @@ new_real_estate = json.dumps({ "role": "boss",
                         }
                     })
 
+#this will fail
+new_real_estate2 = json.dumps({ "role": "boss",
+                        "key": "real estate",
+                        "data": {
+                            "real_estate_id": "huhuhu",
+                            "address": "hverfi skata 2",
+                            "destination": "Reykjavík",
+                            "maintenance_info": "fix this skate",
+                            "tickets": "[]",
+                            "reports": "[]"
+                            }
+                        })
+
 class TestRealEstate(unittest.TestCase):
 
     @classmethod
     def setUpClass(self) -> None:
         """ þar sem það er ekki delete methood þá verð ég að vera smá sniðugur
-            með hvernig eg set upp testin, byrja a ad gera emp, reyni svo ad gera 
-            annann med sama email, thad á að feila, breyti svo upphaflega gæjanum 
-            þannig að næst þegar þetta keyrir þá er sá gæji "ekki til"
+            með hvernig eg set upp testin, byrja a ad gera real estate, reyni 
+            svo ad gera annað med sama real_estate_id, thad á að feila, breyti 
+            svo upphaflega gæjanum þannig að næst þegar þetta keyrir þá er sá 
+            gæji "ekki til"
         """
         self.LL = LogicAPI()
+        res = json.loads(self.LL.post(new_real_estate1))["data"]["id"]
+        self.id = res
 
 
     def test_post_real_estate(self):
-        res = self.LL.post(new_real_estate)
-        self.assertTrue(res)
+        """Try to post new real estate that violates key constraint"""
+        res = self.LL.post(new_real_estate1)
+        self.assertFalse(res)
 
     def test_put_real_estate(self):
-        pass
+        """We change the real estate we made in setUp"""
+        put_data_1 = json.dumps({"role": "boss",
+                        "key": "real_estate",
+                        "data": {
+                            "id": str(self.id),
+                            "real_estate_id": "onlln",
+                            }
+                        })
+        res = self.LL.put(put_data_1)
+        self.assertTrue(res["type"])
 
     def test_put_real_estate_fail(self):
-        pass
+        """We try to change real estate details as regular employee"""
+        put_data_2 = json.dumps({"role": "employee",
+                        "key": "real_estate",
+                        "data": {
+                            "id": str(self.id),
+                            "real_estate_id": "no id",
+                            }
+                        })
+        res = self.LL.put(put_data_2)
+        self.assertFalse(res)
 
 if __name__ == '__main__':
     unittest.main()
