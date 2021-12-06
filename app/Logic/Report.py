a37666a81d8af8ffa640b3058b4c3c0e2c728e6b
Eythor Mikael <eythor108@gmail.com>
2021-12-06 15:56:51 +0000

    update typehints

--- app/Logic/Report.py
+++ app/Logic/Report.py
@@ -1,20 +1,25 @@
+import json
 from Logic.Base import Base
+json
 
 class Report(Base):
     
     def __init__(self) -> None:
         super().__init__("report", "id")
 
-    def get_all(self, data: str):
+    def get_all(self, data: json) -> json:
         return super().get_all(data)
 
-    def get(self, data: str):
+    def get(self, data: json) -> json:
         return super().get(data)
 
-    def post(self, data: str):
+    def post(self, data: json) -> json:
         return super().post(data)
 
-    def put(self, data: str):
+    def put(self, data: json) -> json:
         return super().put(data)
+
+    def delete(self, data: json) -> json:
+        return super().delete(data)
     
 
