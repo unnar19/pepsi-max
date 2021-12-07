754e35a05f006e78071e3ac1d7b80556f8b75f22
unnar19 <unnar19@ru.is>
2021-12-07 17:02:40 +0000

    initiated real estate ui

--- app/UI/ScreensUI.py
+++ app/UI/ScreensUI.py
@@ -30,6 +30,37 @@ class ScreensUI():
         else: 
             return None, 'Invalid input' #not a menu selection
 
+    def screen_lists_from_all(self, listing_list) -> list:
+        ''' Splits listing list into pages '''
+        maximum = 20
+        page_count = (len(listing_list) // maximum) + 1 
+        if len(listing_list) % maximum == 0:
+            page_count -= 1
+
+        page_list = []
+        offset = 0
+        for page_i in range(page_count):
+            if page_i == page_count-1:
+                last_page = listing_list[(page_i)*offset:]
+                remaining = maximum - len(last_page)
+                if remaining == 0:
+                    remaining = 19
+                [last_page.append(['','','','']) for _ in range(remaining)]
+
+                page_list.append(last_page)
+            else:
+                page_list.append(listing_list[(page_i-1)*offset:maximum + offset])
+                offset += 20
+        return page_list
+
+# =======================================================================================================
+# =======================================================================================================
+# =======================================================================================================
+# =======================================================================================================
+# =======================================================================================================
+# Login and menu
+
+
     def login_screen(self, first_login) -> bool: 
         self.format.subtitle = 'Log-in Screen'
         self.format.reset_title()
@@ -98,28 +129,14 @@ class ScreensUI():
             elif input_int == 4: #log out
                 return False
 
-    def screen_lists_from_all(self, listing_list) -> list:
-        ''' Splits listing list into pages '''
-        maximum = 20
-        page_count = (len(listing_list) // maximum) + 1 
-        if len(listing_list) % maximum == 0:
-            page_count -= 1
+    
 
-        page_list = []
-        offset = 0
-        for page_i in range(page_count):
-            if page_i == page_count-1:
-                last_page = listing_list[(page_i)*offset:]
-                remaining = maximum - len(last_page)
-                if remaining == 0:
-                    remaining = 19
-                [last_page.append(['','','','']) for _ in range(remaining)]
-
-                page_list.append(last_page)
-            else:
-                page_list.append(listing_list[(page_i-1)*offset:maximum + offset])
-                offset += 20
-        return page_list
+# =======================================================================================================
+# =======================================================================================================
+# =======================================================================================================
+# =======================================================================================================
+# =======================================================================================================
+# Employees
 
     def employees_screen(self) -> None:      
         self.format.preview_title = f'{"Name":<30} | {"ID":<5} | {"Phone":<10} | {"Location":<12}'
@@ -316,4 +333,92 @@ class ScreensUI():
             self.filter_str = list_of_commands[input_int]
             listing_list = self.inter.filter_listing(self.filter_str,'destination')
             self.page_list = self.screen_lists_from_all(listing_list)
-            
+    
+# =======================================================================================================
+# =======================================================================================================
+# =======================================================================================================
+# =======================================================================================================
+# =======================================================================================================
+# Realestate
+
+    def real_estate_screen(self):
+        self.format.preview_title = f'{"Name":<30} | {"ID":<5} | {"Phone":<10} | {"Location":<12}'
+        search_str = self.format.styles[0][1:-1]
+        self.format.comment = 'Select an option'
+        list_of_comments = ['Enter search term']
+        curr_page = 1
+        self.emp_list = self.inter.listing_all_employees()
+        id_list = []
+        [id_list.append(employee[1])for employee in self.emp_list]
+        # Make list for each screen
+        self.page_list = self.screen_lists_from_all(self.emp_list)
+
+        while True:
+            if self.filter_str == '':
+                self.format.preview_comment = f'Page {curr_page} of {len(self.page_list)} | Filter: [empty]'
+            self.format.subtitle = 'Menu > Employees'
+            self.format.edit_commands(['Search','Filter','Select','Prev page','Next page','Back'])
+            self.format.apply_styles([0,1,1,1,1,1])
+            self.format.update_text_box(0, search_str)
+
+            self.format.listing_lis = self.page_list[curr_page-1]
+
+            self.format.print_screen()
+            input_str = self.get_input('Input')
+            input_int, type_of_input = self.check_type_of_input(input_str)
+
+            if type(type_of_input) != int:
+                self.format.comment = f'{type_of_input}, Select an option'
+
+            elif type_of_input == 0: #Search (form of accurate filtering)
+                self.format.comment = list_of_comments[input_int]
+                self.format.print_screen()
+                search_str = self.get_input('Text input')
+                self.format.update_text_box(input_int, search_str)
+                self.format.comment = 'Select an option'
+
+            elif type_of_input == 1:
+                if input_int == 1: # Filters
+                    self.format.comment = 'Select a filter'
+                    curr_page = 1
+                    self.filter_screen()
+                    self.format.preview_comment = f'Page {curr_page} of {len(self.page_list)} | Filter: [{self.filter_str}]'
+                    self.format.comment = 'Select an option'
+
+                elif input_int == 2: # Select employee
+                    self.format.comment = 'Enter ID of employee'
+                    self.format.print_screen()
+                    id_input = self.get_input('Input')
+                    if id_input in id_list:
+                        self.profile_screen(id_input)
+                        self.emp_list = self.inter.listing_all_employees()
+                        id_list = []
+                        [id_list.append(employee[1])for employee in self.emp_list]
+                        # Make list for each screen again
+                        self.page_list = self.screen_lists_from_all(self.emp_list)
+                        self.filter_str = ''
+                        curr_page = 1
+
+                    else:
+                        self.format.comment = 'ID not valid, Select an option'
+                    self.format.preview_title = f'{"Name":<30} | {"ID":<5} | {"Phone":<10} | {"Location":<12}'
+                    self.format.preview_comment = f'Page {curr_page} of {len(self.page_list)} | Filter: [{self.filter_str}]'
+                    
+                elif input_int == 3: # Previous Page
+                    if curr_page > 1:
+                        self.format.comment = 'Select an option'
+                        curr_page -= 1
+                    else:
+                        self.format.comment = 'Invalid input, Select an option'
+                
+                elif input_int == 4: # Next Page
+                    if curr_page < len(self.page_list):
+                        self.format.comment = 'Select an option'
+                        curr_page += 1
+                    else:
+                        self.format.comment = 'Invalid input, Select an option'
+
+                elif input_int == 5: #Back (goes to back the the main menu)
+                    self.format.listing_lis = self.format.empty_listing()
+                    self.format.comment = 'Select an option'
+                    return
