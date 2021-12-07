5c929d7a0b688701c4f8cb58e67c251cf4583a15
unnar19 <unnar19@ru.is>
2021-12-07 16:07:45 +0000

    edit employee UI interfacing works

--- app/UI/ScreensUI.py
+++ app/UI/ScreensUI.py
@@ -1,14 +1,13 @@
 from os import name
 from UI.FormatUI import FormatUI
-from Logic.LogicAPI import LogicAPI
 import json
 from UI.InteractionsUI import InteractionsUI
 
 class ScreensUI():
     def __init__(self):
         self.format = FormatUI()
-        self.logic_api = LogicAPI()
         self.inter = InteractionsUI()
+        self.id = 'boss'
 
     def get_input(self, prompt_str):
         return input(f' {prompt_str}: ')
@@ -31,26 +30,6 @@ class ScreensUI():
         else: 
             return None, 'Invalid input' #not a menu selection
 
-    def authenticate_login(self, email, password):
-        data_dict = {
-            'key': 'employee',
-            'data': {
-                'email': email,
-                'password': password
-            }
-        }
-        data = json.dumps(data_dict)
-        response = self.logic_api.authenticate_employee(data)
-        response_dict = json.loads(response)
-
-        if not response_dict['type']:
-            return False, None, None, None
-        
-        id = response_dict['data']['id']
-        role = response_dict['data']['role']
-        name = response_dict['data']['name']
-        return True, id, role, name
-    
     def login_screen(self, first_login) -> bool: 
         self.format.subtitle = 'Log-in Screen'
         self.format.reset_title()
@@ -76,7 +55,7 @@ class ScreensUI():
                 password = self.format.commands['Password'][1][1:-1]
                 print(password)
 
-                logged_in, self.id, self.role, self.name = self.authenticate_login(email, password)
+                logged_in, self.id, self.role, self.name = self.inter.authenticate_login(email, password)
                 if not logged_in:
                     return False
                 first_name = self.name.split(' ')
@@ -192,6 +171,14 @@ class ScreensUI():
                     id_input = self.get_input('Input')
                     if id_input in id_list:
                         self.profile_screen(id_input)
+                        self.emp_list = self.inter.listing_all_employees()
+                        id_list = []
+                        [id_list.append(employee[1])for employee in self.emp_list]
+                        # Make list for each screen again
+                        self.page_list = self.screen_lists_from_all(self.emp_list)
+                        self.filter_str = ''
+                        curr_page = 1
+
                     else:
                         self.format.comment = 'ID not valid, Select an option'
                     self.format.preview_title = f'{"Name":<30} | {"ID":<5} | {"Phone":<10} | {"Location":<12}'
@@ -217,21 +204,96 @@ class ScreensUI():
                     return
 
     def profile_screen(self, id_str):
-        self.format.subtitle = 'Menu > Employees > Select'
-        self.format.edit_commands(['Back'])
-        self.format.apply_styles([1])
         self.format.comment = 'Select an option'
-        self.format.preview_title = 'Employee information'
         self.format.profile = True
-        name, self.format.listing_lis = self.inter.get_individual(id_str)
-        self.format.preview_comment = f"There are 10 days until {name}'s birthday"
         while True:
+            self.format.subtitle = 'Menu > Employees > Select'
+            self.format.edit_commands(['Edit info','Tickets','Back'])
+            self.format.apply_styles([1,1,1])
+            self.format.preview_title = 'Employee information'
+            name, self.format.listing_lis = self.inter.custom_individual_preview(id_str)
+            self.format.preview_comment = f"There are 10 days until {name}'s birthday"
             self.format.print_screen()
             input_str = self.get_input('Input')
             input_int, type_of_input = self.check_type_of_input(input_str)
-            if input_int == 0:
-                self.format.profile = False
-                return
+            if type(type_of_input) != int:
+                self.format.comment = f'{type_of_input}, Select an option'
+            else:
+                if input_int == 0: # Edit info
+                    self.edit_profile(id_str)
+                elif input_int == 1: # Tickets?
+                    self.format.comment = 'Select an option' # ALSO JUST GOES BACK
+                    self.format.profile = False
+                    return
+                elif input_int == 2: # Back
+                    self.format.comment = 'Select an option'
+                    self.format.profile = False
+                    return
+
+    def edit_profile(self, id_str):
+        self.format.subtitle = 'Menu > Employees > Select > Edit info'
+        self.format.edit_commands(['Name','Email','Home phone','Mobile phone','Location','Apply Changes','Cancel'])
+        self.format.apply_styles([0,0,0,0,0,1,1])
+        user_data_dict = self.inter.get_individual(id_str)
+        
+        # Set new values to the original ones
+        new_name = user_data_dict['name']
+        new_email = user_data_dict['email']
+        new_h_phone = user_data_dict['home_phone']
+        new_m_phone = user_data_dict['mobile_phone']
+        new_location = user_data_dict['destination']       
+
+        while True:
+            self.format.update_text_box(0,new_name)
+            self.format.update_text_box(1,new_email)
+            self.format.update_text_box(2,new_h_phone)
+            self.format.update_text_box(3,new_m_phone)
+            self.format.update_text_box(4,new_location)
+
+            self.format.print_screen()
+            input_str = self.get_input('Input')
+            input_int, type_of_input = self.check_type_of_input(input_str)
+            if type(type_of_input) != int:
+                self.format.comment = f'{type_of_input}, Select an option'
+            elif type_of_input == 1:
+                if input_int == 5: # Apply changes
+                    new_data_dict = {'id': user_data_dict['id'],'name':new_name, 'email': new_email, 'home_phone': new_h_phone, 'mobile_phone': new_m_phone, 'destination': new_location}
+                    edit_response = self.inter.edit_employee(self.id, new_data_dict)
+                    if not edit_response:
+                        self.format.comment = 'Unauthorized, Select an option'
+                    else:
+                        name, self.format.listing_lis = self.inter.custom_individual_preview(new_data_dict['id'])
+                        return
+                elif input_int == 6: # Cancel
+                    self.format.comment = 'Select an option'
+                    return
+            elif type_of_input == 0:
+                if input_int == 0: # Name
+                    self.format.comment = 'Enter new name'
+                    self.format.print_screen()
+                    new_name = self.get_input('Text input')
+                    self.format.comment = 'Select an option'
+                elif input_int == 1: # Email
+                    self.format.comment = 'Enter new email address'
+                    self.format.print_screen()
+                    new_email = self.get_input('Text input')
+                    self.format.comment = 'Select an option'
+                elif input_int == 2: # Home phone
+                    self.format.comment = 'Enter new home phone number'
+                    self.format.print_screen()
+                    new_h_phone = self.get_input('Text input')
+                    self.format.comment = 'Select an option'
+                elif input_int == 3: # Mobile phone
+                    self.format.comment = 'Enter new mobile phone number'
+                    self.format.print_screen()
+                    new_m_phone = self.get_input('Text input')
+                    self.format.comment = 'Select an option'
+                elif input_int == 4: # Location
+                    self.format.comment = 'Enter new location'
+                    self.format.print_screen()
+                    new_location = self.get_input('Text input')
+                    self.format.comment = 'Select an option'
+    
 
     def filter_screen(self):
         self.format.subtitle = 'Menu > Employees > Filter'
