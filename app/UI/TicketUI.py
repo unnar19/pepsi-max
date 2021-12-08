a84ddc0887bdd0b1c4ca3b4397bae5f80b437804
jonevert <jonp16@ru.is>
2021-12-08 16:05:02 +0000

    exceptions i logic api

--- app/UI/TicketUI.py
+++ app/UI/TicketUI.py
@@ -1,3 +1,53 @@
+from os import name
+from UI.FormatUI import FormatUI
+from UI.InteractionsUI import InteractionsUI
+
 class TicketUI:
-    def __init__(self) -> None:
-        pass
+    def __init__(self):
+        self.format = FormatUI()
+        self.inter = InteractionsUI()
+        self.id = 'boss'
+
+    def get_input(self, prompt_str):
+        return input(f' {prompt_str}: ')
+
+    def check_type_of_input(self, input_str):
+        if input_str.isdigit():
+            input_int = int(input_str)-1
+            if input_int < len(self.format.commands) and input_int >= 0:
+                list_of_options = list(self.format.commands)
+                if self.format.commands[list_of_options[input_int]][1] == self.format.styles[1]: # Command
+                    return input_int, 1
+                elif self.format.commands[list_of_options[input_int]][1] == self.format.styles[2]: # CheckBox
+                    return input_int, 2
+                elif self.format.commands[list_of_options[input_int]][1] == self.format.styles[3]: # Filled CheckBox
+                    return input_int, 3
+                else: # Textbox
+                    return input_int, 0
+            else:
+                return None, 'Input out of range' #number not in range of options
+        else: 
+            return None, 'Invalid input' #not a menu selection
+
+    def screen_lists_from_all(self, listing_list) -> list:
+        ''' Splits listing list into pages '''
+        maximum = 20
+        page_count = (len(listing_list) // maximum) + 1 
+        if len(listing_list) % maximum == 0:
+            page_count -= 1
+
+        page_list = []
+        offset = 0
+        for page_i in range(page_count):
+            if page_i == page_count-1:
+                last_page = listing_list[(page_i)*offset:]
+                remaining = maximum - len(last_page)
+                if remaining == 0:
+                    remaining = 19
+                [last_page.append(['','','','']) for _ in range(remaining)]
+
+                page_list.append(last_page)
+            else:
+                page_list.append(listing_list[(page_i-1)*offset:maximum + offset])
+                offset += 20
+        return page_list
